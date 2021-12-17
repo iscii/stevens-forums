eed59f1bd1bc919ae5ff77e5eb8debbc46a771e2
g00gol <38109082+g00gol@users.noreply.github.com>
2021-12-17 16:12:05 -0500

    Post styling/added color

--- pages/create_post.js
+++ pages/create_post.js
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from "react";
-
 import Nav from "../components/Nav"
 
 function CreatePost() {
@@ -8,10 +6,10 @@ function CreatePost() {
 			<Nav />
 			<span className="h-full w-full flex flex-col items-center justify-center">
 				<h1 className="text-4xl mb-16">Create Post</h1>
-				<form className="w-3/6 h-3/6 bg-red-300 border border-red-300 rounded-3xl" action="/">
-					<input className="mx-6 mt-6 p-2 w-2/6 border border-gray-300 rounded-lg" type="text" placeholder="Title" /><br />
-					<input className="mx-6 mt-6 p-2 w-4/6 pr-72 pb-72 border border-gray-300 rounded-lg" type="text" placeholder="Text" /><br />
-					<button>Submit</button>
+				<form className="w-3/6 h-3/6 bg-pri-gray border border-pri-gray rounded-3xl" action="/">
+					<input className="mx-6 mt-6 p-2 w-2/6 border border-pri-white rounded-lg" type="text" placeholder="Title" /><br />
+					<input className="mx-6 my-6 p-2 w-4/6 pr-72 pb-72 border border-pri-white border-gray-300 rounded-lg" type="text" placeholder="Text" /><br />
+					<button className="mx-6 p-2 bg-pri-red border border-pri-red rounded-2xl">Submit</button>
 				</form>
 			</span>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
   ],
   theme: {
     colors: {
+      'pri-white': '#ffffff',
       'pri-red': '#9D1535',
       'pri-gray': '#e5e7eb',
+      'dark-gray': '#a0a2a5',
     },
     extend: {},
   },
