153862dee8c829efae249b65f990df546df4a4e8
iscii <issac.zheng9869@gmail.com>
2021-12-28 21:34:59 -0500

    firebase auth working!! think i can get the rest to work too. backend on its way!

--- pages/account.js
+++ pages/account.js
@@ -3,42 +3,65 @@ import Nav from "../components/Nav";
 // https://blog.logrocket.com/building-a-fullstack-application-with-next-js-and-firestore-db/
 // https://www.youtube.com/watch?v=Sdv3bw2rIuQ
 import { auth } from "../lib/firebase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, connectAuthEmulator } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 function Account() {
 	return (
 		<div className="h-full">
 			<Nav />
-			//dogshit layout for firebase testing, revamp later
+			//bad layout for firebase testing, revamp later
 			<span className="h-full w-full flex flex-col items-center justify-center">
 				<h1 className="text-4xl mt-24 mb-8">Login</h1>
-				<form className="w-3/6 h-3/6 bg-pri-gray border border-pri-gray rounded-3xl" action="/">
-					<input className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Email/Username" /><br />
-					<input className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Password" /><br />
-					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl">Submit</button>
-				</form>
+				<div className="w-3/6 h-3/6 bg-pri-gray border border-pri-gray rounded-3xl">
+					<input id="l-email" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Email/Username" /><br />
+					<input id="l-pass" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Password" /><br />
+					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl" onClick={test2}>Submit</button>
+				</div>
 			</span>
 			<span className="h-full w-full flex flex-col items-center justify-center">
 				<h1 className="text-4xl mt-8 mb-8">Sign Up</h1>
-				<form className="w-3/6 h-3/6 bg-pri-gray border border-pri-gray rounded-3xl" action="/">
-					<input className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Email/Username" /><br />
-					<input className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Password" /><br />
-					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl">Submit</button>
-				</form>
+				<div className="w-3/6 h-3/6 bg-pri-gray border border-pri-gray rounded-3xl">
+					<input id="s-email" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Email" /><br />
+					<input id="s-uname" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Username" /><br />
+					<input id="s-pass" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Password" /><br />
+					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl" onClick={test}>Submit</button>
+				</div>
 			</span>
 		</div>
 	)
 }
 
-connectAuthEmulator(auth, "http://localhost:3000")
 const test = async function(){
+  	console.log("hi");
 	try {
-		const cred = await createUserWithEmailAndPassword(auth, "test@gmai.com", "123123");
+		const cred = await createUserWithEmailAndPassword(auth, document.getElementById("s-email").value, document.getElementById("s-pass").value);
+		console.log("success!");
+		console.log(cred);
 	}
 	catch(e){
 		console.log("error: \n" + e);
 	}
 }
-test();
+
+const test2 = async function(){
+	console.log("bye");
+	try {
+		const cred = await signInWithEmailAndPassword(auth, document.getElementById("l-email").value, document.getElementById("l-pass").value);
+		console.log("success!");
+		console.log(cred);
+	}
+	catch(e){
+		console.log("error: \n" + e);
+	}
+}
+
+//SUCCESSSSSS!!!!!! THE AUTH WORKS IN THE EMULATOR. FINALLY. NO WONDER WHAT WAS WRONG
+/* 
+*	emulator auth accounts are separate from the actual auth accounts	
+*	connectAuthEmulator connects to the firebase emulator localhost address, not the website localhost
+*	starts auth emulator so that the auth can connect to it with connectAuthEmulator
+*	$ npm run dev
+*	$ firebase emulators:start --only auth
+*/
 
 export default Account
