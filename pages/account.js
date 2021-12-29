5fb10d5b892e942cb27650524d0e1fa6cc5e1d4a
iscii <issac.zheng9869@gmail.com>
2021-12-29 14:44:18 -0500

    format updates

--- pages/account.js
+++ pages/account.js
@@ -1,7 +1,5 @@
 import Nav from "../components/Nav";
 
-// https://blog.logrocket.com/building-a-fullstack-application-with-next-js-and-firestore-db/
-// https://www.youtube.com/watch?v=Sdv3bw2rIuQ
 import { auth } from "../lib/firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
@@ -9,13 +7,12 @@ function Account() {
 	return (
 		<div className="h-full">
 			<Nav />
-			//bad layout for firebase testing, revamp later
 			<span className="h-full w-full flex flex-col items-center justify-center">
 				<h1 className="text-4xl mt-24 mb-8">Login</h1>
 				<div className="w-3/6 h-3/6 bg-pri-gray border border-pri-gray rounded-3xl">
 					<input id="l-email" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Email/Username" /><br />
 					<input id="l-pass" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Password" /><br />
-					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl" onClick={test2}>Submit</button>
+					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl" onClick={login}>Submit</button>
 				</div>
 			</span>
 			<span className="h-full w-full flex flex-col items-center justify-center">
@@ -24,46 +21,32 @@ function Account() {
 					<input id="s-email" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Email" /><br />
 					<input id="s-uname" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Username" /><br />
 					<input id="s-pass" className="mx-6 mt-6 p-2 w-2/6 border border-pri-white text-lg rounded-lg" type="text" placeholder="Password" /><br />
-					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl" onClick={test}>Submit</button>
+					<button className="mx-6 my-6 px-4 py-2 text-pri-white text-lg bg-pri-red border border-pri-red rounded-2xl" onClick={signup}>Submit</button>
 				</div>
 			</span>
 		</div>
 	)
 }
 
-//uncomment to test backend
-/* const test = async function(){
-  	console.log("hi");
+async function login() {
 	try {
-		const cred = await createUserWithEmailAndPassword(auth, document.getElementById("s-email").value, document.getElementById("s-pass").value);
+		const creds = await signInWithEmailAndPassword(auth, document.getElementById("l-email").value, document.getElementById("l-pass").value);
 		console.log("success!");
-		console.log(cred);
 	}
-	catch(e){
+	catch (e) {
+		// have a text box stating issue switch-case based on error (invalid email, invalid password, user not found, etc); 
 		console.log("error: \n" + e);
 	}
 }
 
-const test2 = async function(){
-	console.log("bye");
+async function signup() {
 	try {
-		const cred = await signInWithEmailAndPassword(auth, document.getElementById("l-email").value, document.getElementById("l-pass").value);
+		const creds = await createUserWithEmailAndPassword(auth, document.getElementById("s-email").value, document.getElementById("s-pass").value);
 		console.log("success!");
-		console.log(cred);
 	}
-	catch(e){
+	catch (e) {
 		console.log("error: \n" + e);
 	}
-} */
-
-//SUCCESSSSSS!!!!!! THE AUTH WORKS IN THE EMULATOR. FINALLY. NO WONDER WHAT WAS WRONG
-/* 
-*	emulator auth accounts are separate from the actual auth accounts	
-*	go to http://localhost:4000/auth to see the emulator accounts
-*	connectAuthEmulator connects to the firebase emulator localhost address, not the website localhost
-*	starts auth emulator so that the auth can connect to it with connectAuthEmulator
-*	$ npm run dev
-*	$ firebase emulators:start --only auth
-*/
+}
 
 export default Account
