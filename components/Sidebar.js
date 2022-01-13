078b01f29ad3079b4bfb095f9ded4dccf34fa251
g00gol <38109082+g00gol@users.noreply.github.com>
2022-01-13 15:19:07 -0500

    Create Sidebar.js

---
+++ components/Sidebar.js
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+	return (
+		<div className="flex flex-col">
+			<button className="mb-4">Home</button>
+			<button className="mb-4">Home</button>
+			<button className="mb-4">Home</button>
+		</div>
+	)
+}
