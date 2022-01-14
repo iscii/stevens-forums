300966dbb2ffacf4e3ecce372e5a30df43a830fb
iscii <issac.zheng9869@gmail.com>
2022-01-14 15:41:05 -0500

    update

--- components/Nav.js
+++ components/Nav.js
@@ -40,16 +40,18 @@ function Nav() {
 				<Signup />
 			</div>
 
-			<Link href="/account">
-				<a className="text-pri-white">
-					<div className="px-6 py-2 float-right hover:bg-dark-red hover:underline">
-						<span className="font-quicksand text-pri-black text-lg">Name#0000</span>
+			<div className="px-6 py-2 float-right hover:bg-dark-red hover:underline">
+				<button className="w-24 py-1 px-3 text-pri-red bg-pri-white font-bold border-2 border-pri-red rounded-3xl" onClick={toggle}>Log In</button>
+				<button className="w-24 py-1 px-3 ml-4 text-pri-white bg-pri-red font-bold border-2 border-pri-red rounded-3xl" onClick={toggle}>Sign up</button>
+				<Link href="/account">
+					<a className="text-pri-white cursor-pointer">
+						{/* <span className="font-quicksand text-pri-black text-lg">Name#0000</span> */}
 						<svg className="h-16 w-16 inline ml-4 fill-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 							<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
 						</svg>
-					</div>
-				</a>
-			</Link>
+					</a>
+				</Link>
+			</div>
 		</div>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -41,10 +41,11 @@ export default function Home() {
 							<svg className="h-8 w-8 mr-2 fill-pri-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 								<path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
 							</svg>
-							Welcome back!
+							{/* welcome back for those logged in */}
+							Welcome!
 						</span>
 						<Link href="/create_post">
-							<a className="text-xl font-quicksand font-bold bg-pri-white border border-pri-white drop-shadow-lg rounded-3xl px-2 py-1">Create Post</a>
+							<a className="text-xl py-1 px-3 font-quicksand font-bold bg-pri-white border border-pri-white drop-shadow-lg rounded-3xl">Create Post</a>
 						</Link>
 					</div>
 
