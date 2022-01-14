3330545838aa983ba755a775d526be89c17b5c66
g00gol <38109082+g00gol@users.noreply.github.com>
2022-01-14 15:25:59 -0500

    Container for signup popup and image

--- components/Signup.js
+++ components/Signup.js
@@ -1,9 +1,14 @@
+import Image from "next/image"
+
 export default function Signup() {
 	return (
 		<div className="flex justify-center items-center h-screen w-screen bg-pri-black/50">
-			<div>
-				Hi
-			</div>
+			<span className="relative flex justify-center items-center h-3/5 w-2/6 bg-pri-white border border-transparent rounded-tl-xl rounded-bl-xl">
+				<h1>Join our talented community!</h1>
+			</span>
+			<span className="relative h-3/5 w-1/6 flex justify-end border border-transparent rounded-tr-xl rounded-br-xl">
+				<Image className="border border-transparent rounded-tr-xl rounded-br-xl" src="/chrombubble.jpeg" layout="fill" objectFit="cover" />
+			</span>
 		</div>
 	)
 }
