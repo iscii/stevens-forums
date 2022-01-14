d67f3546a29d6f9ae07b642291034acb0bf40f10
iscii <issac.zheng9869@gmail.com>
2022-01-14 14:26:47 -0500

    login/signup buttons

--- components/Nav.js
+++ components/Nav.js
@@ -13,13 +13,13 @@ function Nav() {
 	}
 
 	return (
-		<div className="fixed flex justify-between items-center h-auto w-full bg-pri-white z-10">
+		<div className="font-quicksand fixed flex justify-between items-center h-auto w-full bg-pri-white z-10">
 			<Link href="/">
 				<a>
 					<div className="px-6 py-2 float-left flex items-center justify-center hover:bg-dark-red">
 						<Image src={"/atillalogo.png"} alt="atilla" width={70} height={70} />
-						<p className="font-quicksand font-bold text-3xl text-pri-red pl-4">duck</p>
-						<p className="font-quicksand font-bold text-3xl">forums</p>
+						<p className="font-bold text-3xl text-pri-red pl-4">duck</p>
+						<p className="font-bold text-3xl">forums</p>
 					</div>
 				</a>
 			</Link>
@@ -28,22 +28,29 @@ function Nav() {
 				<Searchbar />
 			</div>
 
-			<button onClick={toggle}>Sign up</button>
 
+			{/* <div className="fixed top-0 w-screen h-screen flex justify-center items-center" style={{ display: showMe ? "flex" : "none" }}>
+				<Login />
+			</div> */}
+			
 			<div className="fixed top-0 w-screen h-screen flex justify-center items-center" style={{ display: showMe ? "flex" : "none" }}>
 				<Signup />
 			</div>
 
-			<Link href="/account">
-				<a className="text-pri-white">
-					<div className="px-6 py-2 float-right hover:bg-dark-red hover:underline">
-						<span className="font-quicksand text-pri-black text-lg">Name#0000</span>
+			{/* gotta get this into account box without interfering with link smhw, prolly will fix when backend integration*/}
+
+			<div className="px-6 py-2 float-right hover:bg-dark-red hover:underline">
+				<button className="w-24 text-pri-red bg-pri-white font-bold py-1 px-3 border-2 border-pri-red rounded-3xl" onClick={toggle}>Log In</button>
+				<button className="w-24 ml-4 text-pri-white bg-pri-red font-bold py-1 px-3 border-2 border-pri-red rounded-3xl" onClick={toggle}>Sign up</button>
+				<Link href="/account">
+					<a className="text-pri-white cursor-pointer">
+						{/* <span className="font-quicksand text-pri-black text-lg">Name#0000</span> */}
 						<svg className="h-16 w-16 inline ml-4 fill-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 							<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
 						</svg>
-					</div>
-				</a>
-			</Link>
+					</a>
+				</Link>
+			</div>
 		</div>
 	)
 }
