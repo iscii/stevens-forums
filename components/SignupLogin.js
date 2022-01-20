baf3beadc83ba5628e8da2de0669b5b980526976
iscii <issac.zheng9869@gmail.com>
2022-01-19 22:50:17 -0500

    mini

--- components/SignupLogin.js
+++ components/SignupLogin.js
@@ -13,36 +13,34 @@ export default function SignupLogin() {
 	{/* Needs optimization, revisit! Toggle for each model defined separately; possibly merge into one toggle function */ }
 	{/* Toggle function for models */ }
 	const [showLogin, setShowLogin] = useState(false);
-	function toggleLogin() {
-		setShowLogin(!showLogin);
-	}
 	const [showSignup, setShowSignup] = useState(false);
-	function toggleSignup() {
-		setShowSignup(!showSignup);
-	}
-
-	{/* Switch function for models */ }
-	function swapToggle() {
-		setShowLogin(!showLogin);
-		setShowSignup(!showSignup);
+	function toggle(login, signup) {
+		if (login) 
+			setShowLogin(!showLogin);
+		//include back button to email -> put the style display changes into its own function...
+		if (signup) {
+			document.getElementById("s-pri").style.display = "flex";
+			document.getElementById("s-sec").classList.replace("flex", "hidden");
+			setShowSignup(!showSignup);
+		}
 	}
 
 	return (
 		<>
-			<button className="w-24 py-1 px-3 text-lg text-pri-red bg-pri-white font-bold border border-pri-red rounded-full" onClick={toggleLogin}>Log In</button>
-			<button className="w-24 py-1 px-3 ml-4 text-lg text-pri-white bg-pri-red font-bold border border-pri-red rounded-full" onClick={toggleSignup}>Sign up</button>
+			<button className="w-24 py-1 px-3 text-lg text-pri-red bg-pri-white font-bold border border-pri-red rounded-full" onClick={() => toggle(true)}>Log In</button>
+			<button className="w-24 py-1 px-3 ml-4 text-lg text-pri-white bg-pri-red font-bold border border-pri-red rounded-full" onClick={() => toggle(false, true)}>Sign up</button>
 
 			{/* Log in */}
 			<div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center" style={{ display: showLogin ? "flex" : "none" }}>
 				{/* Dark overlay, clicking on overlay will toggle the model */}
-				<a className="fixed h-screen w-screen" onClick={toggleLogin}>
+				<a className="fixed h-screen w-screen" onClick={() => toggle(true)}>
 					<div className="fixed h-screen w-screen bg-pri-black/50"></div>
 				</a>
 
 				{/* Container for the model */}
 				<span className="relative flex flex-col justify-start items-center h-4/6 w-2/6 bg-pri-white border-0 border-transparent rounded-tl-xl rounded-bl-xl">
 					{/* Basic `x` button */}
-					<button className="self-start m-5" onClick={toggleLogin}>
+					<button className="self-start m-5" onClick={() => toggle(true)}>
 						<svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 							<path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
 						</svg>
@@ -63,7 +61,7 @@ export default function SignupLogin() {
 						<p className="text-sm">Forget something? <Link href="/"><a className="text-hyperlink underline">Click here!</a></Link></p>
 					</div>
 					<button className="bg-pri-red px-6 py-2 text-lg text-pri-white border rounded-full">Log in</button>
-					<p className="mt-20">Oops, haven't signed up? <a className="text-hyperlink underline cursor-pointer" onClick={swapToggle}>Sign up here!</a></p>
+					<p className="mt-20">Oops, haven't signed up? <a className="text-hyperlink underline cursor-pointer" onClick={() => toggle(true, true)}>Sign up here!</a></p>
 				</span>
 
 				{/* Side image */}
@@ -77,13 +75,13 @@ export default function SignupLogin() {
 			{/* Sign up */}
 			<div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center" style={{ display: showSignup ? "flex" : "none" }}>
 				{/* Dark overlay, clicking on overlay will toggle the model */}
-				<a className="fixed h-screen w-screen" onClick={toggleSignup}>
+				<a className="fixed h-screen w-screen" onClick={() => toggle(false, true)}>
 					<div className="fixed h-screen w-screen bg-pri-black/50"></div>
 				</a>
 
 				<span className="relative flex flex-col justify-start items-center h-4/6 w-2/6 bg-pri-white border-0 border-transparent rounded-tl-xl rounded-bl-xl">
 					{/* Basic `x` button */}
-					<button className="self-start m-5" onClick={toggleSignup}>
+					<button className="self-start m-5" onClick={() => toggle(false, true)}>
 						<svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 							<path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
 						</svg>
@@ -105,13 +103,14 @@ export default function SignupLogin() {
 					<div id="s-pri" className="w-full flex flex-col justify-start items-center"> {/* using id since we're using an event to modify ui. if there is a better way do revise */}
 						<input id="s-email" className="bg-gray-100 w-5/12 p-3 mb-6 text-pri-black placeholder-pri-black" type="text" placeholder="Stevens E-mail" />
 						<button className="bg-pri-red px-6 py-2 text-lg text-pri-white border rounded-full" onClick={validateemail}>Next</button>
-						<p className="mt-20">Already part of our community? <a className="text-hyperlink underline cursor-pointer" onClick={swapToggle}>Log in here!</a></p>
+						<p className="mt-20">Already part of our community? <a className="text-hyperlink underline cursor-pointer" onClick={() => toggle(true, true)}>Log in here!</a></p>
 					</div>
 
+					{/* TODO: slide-fade in/out animation? */}
 					{/* Signup password field */}
 					<div id="s-sec" className="w-full hidden flex-col justify-start items-center">
-						<input id="s-email" className="bg-gray-100 w-5/12 p-3 mb-6 text-pri-black placeholder-pri-black" type="text" placeholder="Stevens E-mail" />
-						<button className="bg-pri-red px-6 py-2 text-lg text-pri-white border rounded-full" onClick={validateemail}>Next</button>
+						<input id="s-email" className="bg-gray-100 w-5/12 p-3 mb-6 text-pri-black placeholder-pri-black" type="text" placeholder="New Password" />
+						<button className="bg-pri-red px-6 py-2 text-lg text-pri-white border rounded-full" onClick={signup}>Next</button>
 					</div>
 				</span>
 
@@ -150,17 +149,15 @@ async function logout() {
 //could have this trigger state change to show password form instead, but the condition remains the same
 async function validateemail() {
 	let email = document.getElementById("s-email").value;
-	if(email.toLowerCase()
-	.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) //is valid email
-	&& email.includes("@stevens.edu")){
+	if (email.toLowerCase()
+		.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) //is valid email
+		&& email.includes("@stevens.edu")) {
 		document.getElementById("s-pri").style.display = "none";
-		document.getElementById("s-sec").style.className = "s-sec-anim"; //animation class (?) - slide in from right when email box disappears
+		document.getElementById("s-sec").classList.replace("hidden", "flex");
 	}
-	console.log(
-		email.toLowerCase()
-	   .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) //is valid email
-	   && email.includes("@stevens.edu") //is a stevens email
-	);
+	else{
+		//add textbox when invalid. could split the conditional into two if we want it to be more user-friendly
+	}
 }
 
 async function signup() {
